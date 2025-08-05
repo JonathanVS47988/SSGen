944d07042e94640b92a9aade221fb377736856f7
Nightbreaker007 <bootdev.habitat802@passmail.net>
2025-08-05 09:54:10 -0700

    Final Commit before Launch to WWW

--- src/generate_page.py
+++ src/generate_page.py
@@ -17,7 +17,7 @@ def generate_page(from_path, template_path, dest_path):
     with open(template_path) as file:
         template_content = file.read()
         final_content = template_content.replace("{{ Title }}", page_title)
-        final_content = template_content.replace("{{ Content }}", Master_string)
+        final_content = final_content.replace("{{ Content }}", Master_string)
     
     dest_dir = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir):
@@ -25,3 +25,28 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, 'w') as file:
         file.write(final_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    
+    dirs = [item for item in os.listdir(dir_path_content) if os.path.isdir(os.path.join(dir_path_content, item))]
+    files = [item for item in os.listdir(dir_path_content) if os.path.isfile(os.path.join(dir_path_content, item))]
+    template = os.path.abspath(template_path)
+
+    #print(dirs)     #debug
+    #print(files)    #debug
+    #print(template) #debug
+
+    for file in files:
+        try:
+            if os.path.splitext(file)[1] == ".md":
+                base_name, old_ext = os.path.splitext(file)
+                new_file = base_name + ".html"
+                generate_page(os.path.join(dir_path_content, file), template, os.path.join(dest_dir_path, new_file))
+        except:
+            print(f"An error occured while converting the file {file}")
+
+    for dir in dirs:
+        try:
+            generate_pages_recursive(os.path.join(dir_path_content, dir), template, os.path.join(dest_dir_path, dir))
+        except:
+            print(f"An error occured while crawling directory {dir}")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from textnode import TextNode, TextType
 from copy_static import copy_src_to_dest
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 def main():
     #FirstObj = TextNode("test text!", TextType.LINK, "https://www.boot.dev")
     #print(FirstObj)
 
     copy_src_to_dest("./static", "./public")
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content/", "./template.html", "./public/")
 
 main()
